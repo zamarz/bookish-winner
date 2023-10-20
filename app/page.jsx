20d66c3cc70a6c209ceb9b8169cea3050a5718df
zamarz <zachmarzouk@gmail.com>
2023-10-20 10:58:46 +0100

    Initial Home Page

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Use this open-source AI prompting tool to discover, create, and share
+        creative prompts
+      </p>
+    </section>
+  );
+};
+
+export default Home;
