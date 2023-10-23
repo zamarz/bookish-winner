662ef7613311e6bf28bc1883ad82062a20b37c2f
zamarz <zachmarzouk@gmail.com>
2023-10-23 14:59:48 +0100

    "create post form"

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { Router, useRouter } from "next/router";
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  return <div>Create Prompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
